28ab680d104bc359d300169a3bf30d4e79048613
abubakarmana01 <ym.abubakr7@gmail.com>
2022-01-04 23:10:44 +0100

    Fixed banner issue

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,21 +5,21 @@ export default function Banner() {
 	const bannerRef = useRef(undefined);
 
 	useEffect(() => {
-		bannerRef?.current?.style.padding =
+		bannerRef.current.style.padding =
 			window.innerWidth < 550 ? "0 1rem 0" : "15rem 3rem 6rem";
-		bannerRef?.current?.style.height =
+		bannerRef.current.style.height =
 			window.innerWidth < 550 ? "70vh" : "fit-content";
 
 		window.onresize = () => {
-			bannerRef?.current?.style.padding =
+			bannerRef.current.style.padding =
 				window.innerWidth < 550 ? "7.5rem 1rem 6rem" : "15rem 3rem 6rem";
-			bannerRef?.current?.style.height =
+			bannerRef.current.style.height =
 				window.innerWidth < 550 ? "70vh" : "fit-content";
 		};
 
 		return () => {
-			window.onresize = () => {}
-		}
+			window.onresize = () => {};
+		};
 	}, []);
 
 	return (
